f84539eb6ec5438ee114bb5207accc7489bc63cb
RacquelC <reed.rose.rachel@hotmail.com>
2023-03-12 15:05:16 +0100

    changed yml file to run tests

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 import os
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-import pytest
-from selenium.webdriver.support.wait import WebDriverWait
 
-
-
-
-#you can use the beta version instead for path
 os.environ['PATH'] += r"C:/Users/reedr/Documents/selenium drivers"
 driver = webdriver.Chrome()
